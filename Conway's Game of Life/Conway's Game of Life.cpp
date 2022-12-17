67f0d52a0cc0a2c18d52f91a8790c199373421dc
Александр <alexgrihalex@gmail.com>
2022-12-17 14:09:10 +0700

    Base
    A base has been made to which the GUI will be connected.

---
+++ Conway's Game of Life/Conway's Game of Life.cpp
@@ -0,0 +1,147 @@
+﻿#include <iostream>
+#include <vector>
+
+class Rules {
+public:
+	Rules(std::vector<char> arr1, std::vector<char> arr2) {
+		this->born = arr1;
+		this->survive = arr2;
+	}
+	~Rules() {
+
+	}
+
+	std::vector<char> born;
+	std::vector<char> survive;
+};
+
+class Field {
+public:
+	Field(int W, int H) {
+		h = H;
+		w = W;
+		field = new char* [H];
+		for (int i = 0; i < H; i++)
+		{
+			field[i] = new char[W];
+		}
+		for (int i = 0; i < H; i++)
+			for (int j = 0; j < W; j++) {
+				field[i][j] = '0'; //rand() % 2 + '0';
+			}
+		////Blinker (period 2)
+		//field[0][0] = '1';
+		//field[0][1] = '1';
+		//field[0][2] = '1';
+
+		////Toad (period 2)
+		//field[3][3] = '1';
+		//field[3][4] = '1';
+		//field[4][3] = '1';
+		//field[4][4] = '1';
+
+		////Beacon (period 2)
+		//field[3][3] = '1';
+		//field[3][4] = '1';
+		//field[4][3] = '1';
+		//field[4][4] = '1';
+		//field[5][5] = '1';
+		//field[5][6] = '1';
+		//field[6][5] = '1';
+		//field[6][6] = '1';
+
+		////Glider
+		//field[2][4] = '1';
+		//field[3][5] = '1';
+		//field[4][3] = '1';
+		//field[4][4] = '1';
+		//field[4][5] = '1';
+
+	}
+
+	~Field() {
+		delete[] field;
+	}
+
+	void printField() {
+		using namespace std;
+		for (int i = 0; i < this->h; i++)
+		{
+			for (int j = 0; j < this->w; j++)
+			{
+				cout << this->field[i][j];
+			}
+			cout << endl;
+		}
+	}
+
+	void updateState(const Rules &rules) {
+		char** new_field = new char* [this->w];
+		for (int i = 0; i < this->h; i++)
+		{
+			new_field[i] = new char[this->w];
+		}
+		for (int i = 0; i < this->h; i++)
+			for (int j = 0; j < this->w; j++) {
+				new_field[i][j] = '0';
+			}
+		for (int x = 0; x < this->w; x++)
+		{
+			for (int y = 0; y < this->h; y++)
+			{
+				int neighbour_count = 0;
+				for (int i = -1; i <= 1; i++)
+				{
+					for (int j = -1; j <= 1; j++)
+					{
+						if (!i && !j) continue;
+						int neighbour_x = x + i, neighbour_y = y + j;
+						if (neighbour_x < 0) neighbour_x = this->w - 1;
+						if (neighbour_y < 0) neighbour_y = this->h - 1;
+						if (neighbour_x >= this->w) neighbour_x = 0;
+						if (neighbour_y >= this->h) neighbour_y = 0;
+
+						int neighbour = field[neighbour_y][neighbour_x];
+						if (neighbour == '1') ++neighbour_count;
+					}
+				}
+				for (int i = 0; i < rules.survive.size(); i++)
+				{
+					if (neighbour_count == rules.survive[i] && field[y][x] == '1')
+					{
+						new_field[y][x] = '1';
+						continue;
+					}
+				}
+				for (int i = 0; i < rules.born.size(); i++)
+				{
+					if (neighbour_count == rules.born[i]) new_field[y][x] = '1';
+				}
+			}
+		}
+		delete[] field;
+		field = new_field;
+	}
+private:
+	char** field;
+	int h;
+	int w;
+};
+
+int main()
+{
+	const int W = 20, H = 15;
+	Field field = Field(W, H);
+	char x[1] = { 3 };
+	std::vector<char> born(x, x + sizeof x / sizeof x[0]);
+	char y[2] = { 2, 3 };
+	std::vector<char> survive(y, y + sizeof y / sizeof x[0]);
+	Rules rules = Rules(born, survive);
+	field.printField();
+	for (int i = 0; i < 2; i++)
+	{
+		field.updateState(rules);
+		std::cout << '\n';
+		field.printField();
+	}
+}
